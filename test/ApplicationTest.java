f0d8b77d0ee377d6d99d76233431694a253735b1
Thomas McNally <tk.mcnally@gmail.com>
2015-07-27 17:54:52 -0400

    Bootstrap-Agency template integrated.

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -35,7 +35,7 @@ public class ApplicationTest {
 
     @Test
     public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
+        Content html = (Content) views.html.index.render("Your new application is ready.");
         assertThat(contentType(html)).isEqualTo("text/html");
         assertThat(contentAsString(html)).contains("Your new application is ready.");
     }
